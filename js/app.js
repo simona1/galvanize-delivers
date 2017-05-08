71562063239f958a3fc9e8c45e77e48b60c78256
Stela Palii <scientia.ssp@gmail.com>
2017-05-07 23:59:20 -0700

    Menu done

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
 const FOOD_CHOICES = [
-  {name: 'Royale with Cheese', price: '$8.99', picture: 'img/burger.jpg'},
-  {name: 'Arugula Pie', price: '$11.99', picture: 'img/pizza.jpg'},
-  {name: 'Smoked Swine', price: '$14.99', picture: 'img/ribs.jpg'},
-  {name: 'Ice Cream Biscuit', price: '$7.99', picture: 'img/ice_cream.jpg'},
+  {name: 'Royale with Cheese', price: 8.99, imageUrl: 'img/burger.jpg'},
+  {name: 'Arugula Pie', price: 11.99, imageUrl: 'img/pizza.jpg'},
+  {name: 'Smoked Swine', price: 14.99, imageUrl: 'img/ribs.jpg'},
+  {name: 'Ice Cream Biscuit', price: 7.99, imageUrl: 'img/ice_cream.jpg'},
 ];
 
-/*
-function createMenu() {
-
-
-}
-*/
-
-
-
-
-
-
-function makeTagWithClass(tag, name) {
-  return $(tag).addClass(name);
+function makeItemOrderButton() {
+  return $('<div>')
+    .addClass('card-action')
+    .append(
+      $('<a>')
+        .attr('href', '#')
+        .text('Add to order')
+    );
 }
 
-
-function createCard(cardInfo) {
-  const card = makeTagWithClass('div', 'card');
-
-  const cardImage = makeTagWithClass('div', 'card-image');
-  const imageSource = ['<img src=', cardInfo.picture, '>'].join();
-  const imageTag = $(imageSource);
-
-  const cardTitle = makeTagWithClass('span', 'card-title').append(cardInfo.name);
-  const itemPrice = makeTagWithClass('span', 'price').append(cardInfo.price);
-
-  const a = $('<a>').append('Add to order').attr('href', "#");
-  const cardAction = makeTagWithClass('div', 'card-action').append(a);
-  return card;
+function makeItemDescription(name, price) {
+  return $('<div>')
+    .addClass('card-content')
+    .append(
+      $('<span>')
+        .addClass('card-title')
+        .text(name)
+    )
+    .append('$' + price);
 }
 
-/*
-<div class="row">
-  <div class="col s12 m7">
-    <div class="card">
-      <div class="card-image">
-        <img src="img/pizza.jpg">
-        <span class="card-title">Arugula Pie</span>
-        <span class="price">Price</span>
-      </div>
-      <div class="card-action">
-        <a href="#">Add to order</a>
-      </div>
-    </div>
-  </div>
-</div>
-*/
+function makeItemImage(imageUrl) {
+  return $('<div>')
+    .addClass('card-image')
+    .append($('<img>').attr('src', imageUrl));
+}
+
+function makeItemCard(item) {
+  return $('<div>')
+    .addClass('col s12 l6 item')
+    .append(
+      $('<div>')
+        .addClass('card small')
+        .append(makeItemImage(item.imageUrl))
+        .append(makeItemDescription(item.name, item.price))
+        .append(makeItemOrderButton())
+    );
+}
+
+function init() {
+  const rows = [];
+  FOOD_CHOICES.map(makeItemCard).forEach((item, i) => {
+    if (i % 2 === 0) {
+      rows.push($('<div>').addClass('row'));
+    }
+    rows[rows.length - 1].append(item);
+  });
+  $('#menu').append(rows);
+}
+
+window.onload = init;
